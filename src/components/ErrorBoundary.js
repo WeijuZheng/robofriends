0370e2968e1b346380cfd5c81e07ae2e080726e6
weiju <weijuzheng1995@gmail.com>
2020-12-11 15:52:17 -0800

    added error boundary

---
+++ src/components/ErrorBoundary.js
@@ -0,0 +1,24 @@
+const { Component } = require("react");
+
+class ErrorBoundary extends Component {
+    constructor(props) {
+        super();
+        this.state = {
+            hasError: false
+        };
+    }
+
+    componentDidCatch(error, info) {
+        this.setState({ hasError: true });
+    }
+
+    render() {
+        if (this.state.hasError) {
+            return <h1>Something went wrong</h1>
+        } else {
+            return this.props.children;
+        }
+    }
+}
+
+export default ErrorBoundary;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,7 @@
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundary from '../components/ErrorBoundary';
 import { Component } from 'react';
 import './App.css';
 
@@ -42,7 +43,9 @@ class App extends Component {
                     <h1 className='f1'>RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange} />
                     <Scroll>
-                        <CardList robots={filteredRobots} />
+                        <ErrorBoundary>
+                            <CardList robots={filteredRobots} />
+                        </ErrorBoundary>
                     </Scroll>
                 </div>
             );
